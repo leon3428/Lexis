3a711590f4717e8722cf60c58afe8712ebab8cd8
leon3428 <leon.stjepan@gmail.com>
2022-10-15 18:16:49 +0200

    dfa interface update

--- src/Dfa.cpp
+++ src/Dfa.cpp
@@ -1,28 +1,35 @@
 #include "Dfa.hpp"
 #include "Utils.hpp"
 
-Dfa::Dfa(int stateCnt) 
-    : m_stateCnt(stateCnt), m_alphabetSize(100)
+int Dfa::m_getSymbolId(char symbol)
 {
-    m_transitionTable = new int[stateCnt * m_alphabetSize];
-    m_acceptable = new int[stateCnt];
+    int ret = symbol - ' ';
+
+    if(symbol == '\t')
+        ret = '~' - ' ' + 1;
+    if(symbol == '\n')
+        ret = '~' - ' ' + 2;
+    
+    DebugAssert(ret < 0 || ret >= m_alphabetSize, "Symbol out of range");
+    return ret;
 }
 
-Dfa::~Dfa() {
-    delete[] m_transitionTable;
-    delete[] m_acceptable;
+void Dfa::setTransition(const int state, const char symbol, const int nextState) { 
+    // grow dfa if necessary
+    while(m_acceptable.size() <= state) {
+        m_transitionTable.emplace_back();
+        m_acceptable.push_back(-1);
+    }
+
+    m_transitionTable[state][m_getSymbolId(symbol)] = nextState; 
 }
 
-int Dfa::getTransition(int state, char symbol) const {
-    DebugAssert(symbol < ' ' || symbol > '~', "Invalid symbol");
-    DebugAssert(state < 0 || state >= m_stateCnt, "Invalid state");
+void Dfa::setAcceptable(int state, int val) {
+    // grow dfa if necessary
+    while(m_acceptable.size() <= state) {
+        m_transitionTable.emplace_back();
+        m_acceptable.push_back(-1);
+    }
 
-    return m_transitionTable[state * m_alphabetSize + symbol - ' '];
-}
-
-void Dfa::setTransition(int state, char symbol, int nextState) {
-    DebugAssert(symbol < ' ' || symbol > '~', "Invalid symbol");
-    DebugAssert(state < 0 || state >= m_stateCnt, "Invalid state");
-
-    m_transitionTable[state * m_alphabetSize + symbol - ' '] = nextState;
+    m_acceptable[state] = val;
 }
--- src/Dfa.hpp
+++ src/Dfa.hpp
@@ -1,18 +1,87 @@
 #pragma once
 
 #include <vector>
+#include <array>
 
+/**
+ * @brief class stores generated dfa
+ * 
+ */
 class Dfa{
-    int m_stateCnt, m_alphabetSize;
+    static const int m_alphabetSize = 100;
     int m_startState;
 
-    int *m_transitionTable;
-    int *m_acceptable;
+    std::vector<std::array<int, m_alphabetSize> > m_transitionTable;
+    std::vector<int> m_acceptable;
+
+    /**
+     * @brief function converts an symbol of the alphabet into it's corrsponding id value used to index the transition table
+     * 
+     * @param symbol all printable characters + TAB + new line
+     * @return int 
+     */
+    static int m_getSymbolId(char symbol);
 
 public:
-    Dfa(int stateCnt);
-    ~Dfa();
+    /**
+     * @brief Get the Start State object
+     * 
+     * @return int 
+     */
+    inline int getStartState() const { return m_startState; }
 
-    int getTransition(int state, char symbol) const;
+    /**
+     * @brief Set the Start State object
+     * 
+     * @param startState 
+     */
+    inline void setStartState(int startState) { m_startState = startState; }
+
+    /**
+     * @brief Get the Transition object
+     * 
+     * @param state 
+     * @param symbol 
+     * @return int 
+     */
+    inline int getTransition(int state, char symbol) const { return m_transitionTable[state][m_getSymbolId(symbol)]; }
+
+    /**
+     * @brief Set the Transition object; dfa will expand if state larger than dfa
+     * 
+     * @param state 
+     * @param symbol 
+     * @param nextState 
+     */
     void setTransition(int state, char symbol, int nextState);
+
+    /**
+     * @brief Get the Acceptable object;
+     * 
+     * @param state 
+     * @return int; -1 if not acceptable; index of a first acceptable regex otherwise 
+     */
+    inline int getAcceptable(int state) const { return m_acceptable[state]; }
+
+    /**
+     * @brief Set the Acceptable object
+     * 
+     * @param state 
+     * @param val -1 if not acceptable; index of a first acceptable regex otherwise 
+     */
+    void setAcceptable(int state, int val);
+
+    /**
+     * @brief Get the State Count of dfa
+     * 
+     * @return int 
+     */
+    inline int getStateCount() const { return m_acceptable.size(); }
+
+    /**
+     * @brief Get the Alphabet Size
+     * 
+     * @return int 
+     */
+    inline int getAlphabetSize() const { return m_alphabetSize; }
 };
