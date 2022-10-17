e2efca6706d8e29a35120ab5dc525e2777c4b561
ivan <ivan.milinovic123@gmail.com>
2022-10-17 12:55:25 +0200

    init

--- src/Dfa.cpp
+++ src/Dfa.cpp
@@ -24,6 +24,16 @@ void Dfa::setTransition(const int state, const char symbol, const int nextState)
     m_transitionTable[state][m_getSymbolId(symbol)] = nextState; 
 }
 
+void Dfa::setTransitionInt(const int state, const int symbol, const int nextState) { 
+    // grow dfa if necessary
+    while(m_acceptable.size() <= state) {
+        m_transitionTable.emplace_back();
+        m_acceptable.push_back(-1);
+    }
+
+    m_transitionTable[state][symbol] = nextState; 
+}
+
 void Dfa::setAcceptable(int state, int val) {
     // grow dfa if necessary
     while(m_acceptable.size() <= state) {
